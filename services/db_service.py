49eb58bc3b137556ae38eacac7b438bfe33c0cfc
ramsesryandineros <ramsesryandineros@gmail.com>
2021-09-08 15:22:42 +0800

    refactor(db_service): remove db_service class, move methods out of class

--- services/db_service.py
+++ services/db_service.py
@@ -4,50 +4,50 @@ from mongoengine.errors import NotUniqueError, DoesNotExist
 from schemas import User, Room
 from exceptions import EmailIsAlreadyTaken, RoomDoesNotExist, RoomHasDuplicateNumberOrName, UserDoesNotExist
 
-class DBService:
-  def initialize_db(self) -> None:
-    print("initializing db...")
-    connect("iqtrace")
 
-  def create_user(self, user) -> str:
-    new_user = User(**user)
-    try:
-      new_user.save()
-    except NotUniqueError:
-      raise EmailIsAlreadyTaken(f"{user['email']} is aleady taken")
-    return str(new_user.pk)
-  
-  def get_user_from_email(self, email) -> User:
-    user = User.objects.get(email=email)
-    return user
-  
-  def get_user_id_from_email(self, email) -> str:
-    user = self.get_user_from_email(email)
-    return str(user.pk)
-  
-  def get_users(self) -> str:
-    return User.objects.to_json()
-  
-  def delete_user(self, id) -> None:
-    try:
-      User.objects.get(id=id).delete()
-    except DoesNotExist:
-      raise UserDoesNotExist(f"User {id} does not exist.")
-  
-  def create_room(self, room) -> str:
-    new_room = Room(**room)
-    try:
-      new_room.save()
-    except NotUniqueError:
-      raise RoomHasDuplicateNumberOrName(
-        f"Room number {room['number']} or {room['name']} is already taken.")
-    return str(new_room.pk)
-  
-  def get_rooms(self):
-    return Room.objects.to_json()
-  
-  def delete_room(self, room_num) -> None:
-    try:
-      Room.objects.get(number=room_num).delete()
-    except DoesNotExist:
-      raise RoomDoesNotExist(f"Room {room_num} does not exist.")
+def initialize_db() -> None:
+  print("initializing db...")
+  connect("iqtrace")
+
+def create_user(user) -> str:
+  new_user = User(**user)
+  try:
+    new_user.save()
+  except NotUniqueError:
+    raise EmailIsAlreadyTaken(f"{user['email']} is aleady taken")
+  return str(new_user.pk)
+
+def get_user_from_email(email) -> User:
+  user = User.objects.get(email=email)
+  return user
+
+def get_user_id_from_email(email) -> str:
+  user = get_user_from_email(email)
+  return str(user.pk)
+
+def get_users() -> str:
+  return User.objects.to_json()
+
+def delete_user(id) -> None:
+  try:
+    User.objects.get(id=id).delete()
+  except DoesNotExist:
+    raise UserDoesNotExist(f"User {id} does not exist.")
+
+def create_room(room) -> str:
+  new_room = Room(**room)
+  try:
+    new_room.save()
+  except NotUniqueError:
+    raise RoomHasDuplicateNumberOrName(
+      f"Room number {room['number']} or {room['name']} is already taken.")
+  return str(new_room.pk)
+
+def get_rooms():
+  return Room.objects.to_json()
+
+def delete_room(room_num) -> None:
+  try:
+    Room.objects.get(number=room_num).delete()
+  except DoesNotExist:
+    raise RoomDoesNotExist(f"Room {room_num} does not exist.")
--- tests/test_db_service.py
+++ tests/test_db_service.py
@@ -5,8 +5,8 @@ import pytest
 from mongoengine import connect, disconnect
 from mongoengine.errors import DoesNotExist
 
+import services.db_service as db
 from schemas import User, Room
-from services.db_service import DBService
 from exceptions import (EmailIsAlreadyTaken, RoomDoesNotExist,
   RoomHasDuplicateNumberOrName, UserDoesNotExist)
 
@@ -28,10 +28,6 @@ def room():
     'name': "My Room"
   }
 
-@pytest.fixture
-def db():
-  return DBService()
-
 @pytest.fixture(autouse=True)
 def setup_db():
   connect("mongoenginetest", host="mongomock://localhost")
@@ -40,18 +36,18 @@ def setup_db():
 
   disconnect()
 
-def test__create_user__user_is_created(user, db, setup_db):
+def test__create_user__user_is_created(user, setup_db):
   id = db.create_user(user)
 
   assert User.objects.get(id=id)
 
-def test__create_user__raise_if_email_is_taken(user, db, setup_db):
+def test__create_user__raise_if_email_is_taken(user, setup_db):
   db.create_user(user)
   
   with pytest.raises(EmailIsAlreadyTaken):
     db.create_user(user)
 
-def test__delete_user__user_is_deleted(user, db, setup_db):
+def test__delete_user__user_is_deleted(user, setup_db):
   other_user = user.copy()
   other_user['email'] = "ramses@yahoo.com"
   db.create_user(other_user)
@@ -63,29 +59,29 @@ def test__delete_user__user_is_deleted(user, db, setup_db):
   with pytest.raises(DoesNotExist):
     User.objects.get(id=id)
 
-def test__delete_user__nonexistent_user_raises_exception(user, db, setup_db):
+def test__delete_user__nonexistent_user_raises_exception(user, setup_db):
   with pytest.raises(UserDoesNotExist):
     db.delete_user("6123361c16cce88331c423b1")
 
-def test__create_room__creates_room(room, db, setup_db):
+def test__create_room__creates_room(room, setup_db):
   id = db.create_room(room)
 
   assert Room.objects.get(id=id)
 
-def test__create_room__raise_if_room_number_is_taken(room, db, setup_db):
+def test__create_room__raise_if_room_number_is_taken(room, setup_db):
   db.create_room(room)
 
   with pytest.raises(RoomHasDuplicateNumberOrName):
     db.create_room(room)
 
-def test__get_rooms__gets_all_rooms(room, db, setup_db):
+def test__get_rooms__gets_all_rooms(room, setup_db):
   db.create_room(room)
   rooms = json.loads(db.get_rooms())
 
   assert len(rooms) > 0
   assert rooms[0]['number'] == room['number']
 
-def test__delete_room__room_is_deleted(room, db, setup_db):
+def test__delete_room__room_is_deleted(room, setup_db):
   other_room = {
     'number': 1,
     'name': 'Other Room'
@@ -99,6 +95,6 @@ def test__delete_room__room_is_deleted(room, db, setup_db):
   with pytest.raises(DoesNotExist):
     Room.objects.get(number=room['number'])
 
-def test__delete_room__nonexistent_room_raises_exception(room, db, setup_db):
+def test__delete_room__nonexistent_room_raises_exception(setup_db):
   with pytest.raises(RoomDoesNotExist):
     db.delete_room(16)
